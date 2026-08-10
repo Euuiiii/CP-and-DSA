820c3183d159029da977bc60607825c754cd2f5a
Emon Adhikari <102776222+Euuiiii@users.noreply.github.com>
2026-08-10 22:19:36 +0545

    Solved: Remove Element (leetcode)

---
+++ leetcode/Easy/remove-element.cpp
@@ -0,0 +1,22 @@
+// Problem: Remove Element
+// Platform: leetcode
+// Rating/Difficulty: Easy
+// Language: cpp
+// Verdict: Accepted
+// URL: https://leetcode.com/problems/remove-element/
+// Solved on: 2026-08-10T16:34:34.594Z
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int k = 0;
+
+        for(int i =0; i < nums.size(); i++){
+            if(nums[i] != val){
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+};
