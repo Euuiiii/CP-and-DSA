af71626e1c23045e6f9d801f82992601964d6177
Emon Adhikari <emonadhikari321@gmail.com>
2026-08-19 17:36:58 +0545

    Solved: Cinema Seat Allocation (leetcode)

---
+++ leetcode/Medium/cinema-seat-allocation.cpp
@@ -0,0 +1,43 @@
+// Problem: Cinema Seat Allocation
+// Platform: leetcode
+// Rating/Difficulty: Medium
+// Language: cpp
+// Verdict: Accepted
+// URL: https://leetcode.com/problems/cinema-seat-allocation/
+// Solved on: 2026-08-19T11:51:58.042Z
+
+class Solution {
+public:
+    int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
+        unordered_map<int, unordered_set<int>> seats;
+
+        for (auto& r : reservedSeats) {
+            int row = r[0];
+            int seat = r[1];
+
+            if (seat >= 2 && seat <= 5) {
+                seats[row].insert(0);
+            }
+
+            if (seat >= 4 && seat <= 7) {
+                seats[row].insert(1);
+            }
+
+            if (seat >= 6 && seat <= 9) {
+                seats[row].insert(2);
+            }
+        }
+
+        int total = 2 * n;
+
+        for (auto& [row, groups] : seats) {
+            if (groups.size() == 3) {
+                total -= 2;
+            } else {
+                total -= 1;
+            }
+        }
+
+        return total;
+    }
+};
